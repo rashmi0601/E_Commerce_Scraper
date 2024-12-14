a74fb8d579cb48cf8a821772b0ecc660e24ed43a
r_u_0601 <121444395+rashmi0601@users.noreply.github.com>
2024-12-15 00:57:24 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
-Rails.application.config.scraper_api_key = '8091b278d360aee09df33a1c08b2825b'
+Rails.application.config.scraper_api_key = 'xxx'
